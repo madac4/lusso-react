10e86233810b54466de3dcd7a19c41577e43bdb1
Fedotos228 <tudor.onceanu@gmail.com>
2022-10-10 15:22:05 +0300

    clients

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const dataClients = [
+    {
+        ClienName: "Vlada Gurduza",
+        ClientInsta: "example.instagram",
+        ClientReview: "Consequatur laborum nulla blanditiis earum fuga eum autem quae at. Quaerat esse molestiae eligendi et officiis fugit accusantium dolorem. Autem rem quas delectus ab aut. Quos placeat esse vel sed veritatis sit. Perferendis necessitatibus libero. Vel autem voluptates in ut sunt molestiae excepturi est minima.",
+        ClientImg: [
+            "assets/img/client1.jpg",
+            "assets/img/client2.jpg",
+            "assets/img/client3.jpg",
+            "assets/img/client4.jpg",
+            "assets/img/client5.jpg",
+        ]
+    },
+    {
+        ClienName: "Cory Koelpin",
+        ClientInsta: "example.instagram",
+        ClientReview: "A molestiae incidunt voluptas. Dolorem inventore accusantium. Vel at beatae sapiente quis. Fugit officia quae modi et impedit omnis. Eos pariatur labore reiciendis sit velit.",
+        ClientImg: [
+            "assets/img/client1.jpg",
+            "assets/img/client2.jpg",
+            "assets/img/client3.jpg",
+        ]
+    },
+    {
+        ClienName: "Lance Gottlieb",
+        ClientInsta: "example.instagram",
+        ClientReview: "Labore aliquid et natus nobis molestiae at velit et. Iste consequatur expedita repudiandae. Autem ipsum ipsam est vel doloremque ratione. Quod molestiae laborum perferendis facilis dolores sapiente dicta rerum. Harum nisi sit explicabo vel quisquam.",
+        ClientImg: [
+            "assets/img/client1.jpg",
+            "assets/img/client2.jpg",
+            "assets/img/client3.jpg",
+            "assets/img/client4.jpg",
+        ]
+    },
+]
